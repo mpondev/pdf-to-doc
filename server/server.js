ea4f6a4215a935ccde1b2d4eb35510a4f886a2bc
Mario Ponce <mario@mpondev.com>
2023-11-17 21:16:48 +0100

    Start backend. Add express (and setup), dotenv and nodemon

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
+const app = require('./app');
+
+// START SERVER
+const PORT = process.env.PORT || 3006;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}...`);
+});
