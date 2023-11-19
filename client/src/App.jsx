25239ad060e16302d5d38497797d52a64efceada
Mario Ponce <mario@mpondev.com>
2023-11-19 11:53:14 +0100

    First frontend sketch to select a file and submit it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,46 @@
+import { useState } from 'react';
+import './App.css';
+
 function App() {
-  return <div>PDF-TO-DOC</div>;
+  const [file, setFile] = useState(null);
+
+  const selectedHandler = evt => {
+    setFile(evt.target.files[0]);
+  };
+
+  const submitHandler = () => {
+    if (!file) {
+      alert('You must select a file');
+      return;
+    }
+
+    const formdata = new FormData();
+    formdata.append('pdf', file);
+
+    fetch('http://localhost:3005/uploads', {
+      method: 'POST',
+      body: formdata,
+    })
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
+
+    setFile(null);
+  };
+
+  return (
+    <div className="container">
+      <input
+        className="input"
+        name="src-file"
+        type="file"
+        aria-label="File"
+        onChange={selectedHandler}
+      />
+      <button className="button" onClick={submitHandler}>
+        Upload
+      </button>
+    </div>
+  );
 }
 
 export default App;
